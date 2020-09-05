669ed6d66fd3e018f6691351e7ecda7e363d6c9f
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-09-05 19:00:55 +0900

    Initial Commit

---
+++ QuestionApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp1
+//
+//  Created by 山岡巧 on 2020/09/05.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
